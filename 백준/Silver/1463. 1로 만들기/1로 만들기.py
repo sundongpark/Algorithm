bce3cf212f5cfb6e671f23a981c7fdbe7bd1435d
박선동 (Sundong Park) <sundong@khu.ac.kr>
2022-04-12 13:32:28 +0900

    [Silver III] Title: 1로 만들기, Time: 556 ms, Memory: 38652 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,18 @@
+x = int(input())
+
+dp = [0] * 1000001
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
+
+for i in range(4, x + 1):
+    if i % 3 == 0 and i % 2 == 0:
+        dp[i] = min(dp[i-1] + 1, dp[i//2] + 1, dp[i//3] + 1)
+    elif i % 2 == 0:
+        dp[i] = min(dp[i-1] + 1, dp[i//2] + 1)
+    elif i % 3 == 0:
+        dp[i] = min(dp[i-1] + 1, dp[i//3] + 1)
+    else:
+        dp[i] = dp[i-1] + 1
+
+print(dp[x])
