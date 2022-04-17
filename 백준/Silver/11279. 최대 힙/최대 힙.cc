2499c6188f7c50e4fc1d0ddaa48ca4a2e747499e
박선동 (Sundong Park) <sundong@khu.ac.kr>
2022-04-17 21:15:19 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); 
+	cout.tie(NULL);
+
+	int n, a;
+	priority_queue<int, vector<int>, less<int>> q;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+		if (a == 0)
+		{
+			if (q.empty())
+				cout << "0\n";
+			else
+			{
+				cout << q.top() << '\n';
+				q.pop();
+			}
+		}
+		else
+			q.push(a);
+	}
+	return 0;
+}
