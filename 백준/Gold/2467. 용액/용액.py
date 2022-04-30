ae5a54e4d3eb01b6c4b0f74d323e0bc00bc95d71
박선동 (Sundong Park) <sundong@khu.ac.kr>
2022-05-01 08:12:52 +0900

    [Gold V] Title: 용액, Time: 148 ms, Memory: 42240 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+a = list(map(int, input().rstrip().split()))
+l = left = 0
+r = right = n - 1
+min = 2000000001
+while l < r:
+    sum = a[l] + a[r]
+    if abs(sum) < abs(min):
+        left = l
+        right = r
+        min = sum
+    if sum > 0:
+        r = r - 1
+    elif sum < 0:
+        l = l + 1
+    else:
+        break
+
+print(a[left], a[right])
