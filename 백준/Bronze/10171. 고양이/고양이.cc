ec827b693ae957bef3013b4023d9e7f2d34e0aa1
박선동 (Sundong Park) <sundong@khu.ac.kr>
2022-08-03 12:58:41 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+   cout.tie(NULL);
+   cout << "\\    /\\" << '\n';
+   cout << " )  ( ')" << '\n';
+   cout << "(  /  )"<<'\n';
+   cout << " \\(__)|";
+
+   return 0;
+}
