68ba1849a95612d3460af34560cf69ddcded9e2d
박선동 (Sundong Park) <sundong@khu.ac.kr>
2022-08-09 22:44:14 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int a;
+	cin >> a;
+	cout << 1 - a << ' ';
+	cin >> a;
+	cout << 1 - a << ' ';
+	cin >> a;
+	cout << 2 - a << ' ';
+	cin >> a;
+	cout << 2 - a << ' ';
+	cin >> a;
+	cout << 2 - a << ' ';
+	cin >> a;
+	cout << 8 - a;
+	return 0;
+}
