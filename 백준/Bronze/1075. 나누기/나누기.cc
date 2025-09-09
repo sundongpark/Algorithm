748a7aa3ba946993b4fb597d232d1b87069b5215
박선동 (Sundong Park) <sundong@khu.ac.kr>
2025-09-10 01:32:30 +0900

    [Bronze II] Title: 나누기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1075. 나누기/나누기.cc
+++ 백준/Bronze/1075. 나누기/나누기.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
 int main()
@@ -10,14 +9,8 @@ int main()
 
 	int n, f;
 	cin >> n >> f;
-
-	n = n / 100 * 100;
-	int i = 0;
-	for (i = 0; i < 100; i++)
-		if (!((n + i) % f))
-			break;
-
-	cout << setw(2) << setfill('0') << i;
+	int m = n - n % 100;
+	printf("%02d", ((m + f - 1) / f) * f % 100);
 
 	return 0;
 }
