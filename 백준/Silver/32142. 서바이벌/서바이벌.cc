6d4b8dd8a9fd13bfc0ef528427ec81dc943bfecb
박선동 (Sundong Park) <sundong@khu.ac.kr>
2025-10-03 17:42:26 +0900

    [Silver III] Title: 서바이벌, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/32142. 서바이벌/서바이벌.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+    }
+
+    cout << -1 << '\n';
+
+    return 0;
+}
